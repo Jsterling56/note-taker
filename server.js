e7321cd88465e1ad145d27648d46383f32d10ece
JSterling56 <jtsterlin@yahoo.com>
2023-07-10 18:33:55 -0400

    reworked code

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { notes } = require("./db/db");
+let db = require("./db/db.json");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,48 +15,72 @@ app.use(express.static('public'));
 const generateUniqueId = require('generate-unique-id');
 
 //Create New Note
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-  };  
+// function createNewNote(body, notesArray) {
+//     const note = body;
+//     notesArray.push(note);
+//     fs.writeFileSync(
+//       path.join(__dirname, './db/db.json'),
+//       JSON.stringify({ notes: notesArray }, null, 2)
+//     );
+//     return note;
+//   };  
 
 //Routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+  db = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+  res.json(db);
 });
 
 app.post('/api/notes', (req, res) => {
-  req.body.id = generateUniqueId();
-  const note = createNewNote(req.body, notes);
-  res.json(note);
+  // req.body.id = generateUniqueId();
+  const note = {
+    title: req.body.title,
+    text: req.body.text,
+    id: generateUniqueId()
+  }
+  db.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(db, null, 2)
+  );
+  db = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+  res.json(db);
 });
-    
+
 //Bonus - DELETE 
 //NOTE: Found this code almost exactly as needed here: https://www.tabnine.com/code/javascript/functions/express/Express/delete
 // I'm probably going to use this a lot in the future because wow what a resource!!
 app.delete('/api/notes/:id', (req, res) => {
-  const { id } = req.params;
+  // const { id } = req.params;
 
-  const delNote = notes.findIndex(note => note.id ==id);
-
-  notes.splice(delNote, 1);
-  return res.send();
+  // db.filter(note => note.id == id);
+  const notesToKeep = []
+  for (var i = 0; i < db.length; i++){
+    if (db[i].id != req.params.id){
+      notesToKeep.push(db[i]);
+    }
+  }
+  db = notesToKeep;
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(db, null, 2)
+  );
+  db = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+  res.json(db);
+  
+  // db.splice(delNote, 1);
+  // return res.send();
 });
 
 //Listener
 app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}`);
-  });
+  console.log(`App listening on PORT ${PORT}`);
+});
